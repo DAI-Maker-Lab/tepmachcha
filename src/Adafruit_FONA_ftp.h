ce41e7001be929ad4bc6b5177521a45f014dd6dc
jtheorent <jack@theorent.com>
2017-05-25 17:38:30 +0700

    Fix lat-block size and \r\n eater for ftpcopyfile, shorted log messages, misc tidying

--- src/Adafruit_FONA_ftp.h
+++ src/Adafruit_FONA_ftp.h
@@ -4,6 +4,8 @@ class Adafruit_FONA_ftp : public Adafruit_FONA
 {
   public:
     Adafruit_FONA_ftp(int8_t);
-    void showline(char *);
-    boolean Adafruit_FONA_ftp::getFtp(void);
+    /*
+    boolean Adafruit_FONA_ftp::sendCheckOK(FONAFlashStringPtr, uint16_t);
+    boolean Adafruit_FONA_ftp::sendCheckOK(char*, uint16_t);
+    */
 };
